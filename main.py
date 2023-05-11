2979f769a146095e47fdd8d92545384bc0c9bb12
M4hfd <83091151+M4hfd@users.noreply.github.com>
2023-05-11 17:00:43 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import pdfplumber
 from gtts import gTTS
 
+
 def get_text_string():
     file_path = "text.pdf"
     with pdfplumber.PDF(open(file_path, mode='rb')) as pdf_file:
         page = pdf_file.pages
         pages_pdf_text = ''.join([page.extract_text() for page in page])
         pdf_text = "".join(pages_pdf_text)
+        pdf_file.close()
         return(pdf_text.replace("\n", " "))
 
 
@@ -23,4 +25,4 @@ def main():
 
 
 if __name__ == '__main__':
-		main()
+    main()
