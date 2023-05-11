869148bc54344f5d710f067b8312ade5bf9dafb5
M4hfd <83091151+M4hfd@users.noreply.github.com>
2023-05-11 16:24:42 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+import pdfplumber
+from gtts import gTTS
+
+def get_text_string():
+    file_path = "text.pdf"
+    with pdfplumber.PDF(open(file_path, mode='rb')) as pdf_file:
+        page = pdf_file.pages
+        pages_pdf_text = ''.join([page.extract_text() for page in page])
+        pdf_text = "".join(pages_pdf_text)
+        return(pdf_text.replace("\n", " "))
+
+
+def get_audio_file():
+    language = "ru"
+    pdf_text = get_text_string()
+    phrase = gTTS(text=pdf_text, lang=language, slow=False)
+    phrase.save("computerhistory.mp3")
+
+
+def main():
+    get_text_string()
+    get_audio_file()
+
+
+if __name__ == '__main__':
+		main()
